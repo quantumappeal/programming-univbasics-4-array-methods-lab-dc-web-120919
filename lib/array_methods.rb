1a65c87855633c6f67a1f04256dae7153759bc9d
Sean Beach <sean.beach1@yahoo.com>
2019-11-07 03:10:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,17 @@
 def using_include(array, element)
-
+  array.include?(wow)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.last
 end
 
 def using_last(array)
